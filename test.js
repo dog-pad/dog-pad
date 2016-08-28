7f2036461b40af697e8d86d6678b56b885aa15f6
Steve Mao <maochenyan@gmail.com>
2016-08-28 15:35:44 +1000

    Woof Woof

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import m from './';
 
 test(t => {
-	t.is(m('Meow', 0), 'Meow');
-	t.is(m('Meow', 1), '🐈Meow');
-	t.is(m('Meow', 2), '🐈🐈Meow');
+	t.is(m('Woof', 0), 'Woof');
+	t.is(m('Woof', 1), '🐈Woof');
+	t.is(m('Woof', 2), '🐈🐈Woof');
 });
